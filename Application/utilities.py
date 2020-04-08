0c512e17801738c36ee9adf943ea5f3ecd8c8e85
Jimmy Lee <46268173+JimmyMinhLee@users.noreply.github.com>
2020-04-08 11:03:16 -0700

    Removed unnecessary files; started storage and integration into Postgre
    Created a csv file that stores the tweet and included a todo list for the future; plans are to plug the application into Postgre directly instead of having to load it in through the CSV file all the time.

--- Application/utilities.py
+++ Application/utilities.py
@@ -1,6 +1,7 @@
 import json, sys
 from os import system
 import numpy as np
+import datetime as dt
 # from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
@@ -25,8 +26,10 @@ class TextProcessor():
         self.average_y_data = [0]
 
     def process(self, raw_data, average_step):
+        csv = open('data.csv', "a")
         text = self.get_text(raw_data)
         # if "RT" in text: return
+        weights = {1:1, 0:1.3}
         response = self.analyzer.polarity_scores(text)['compound']
         print(text)
 
@@ -41,29 +44,8 @@ class TextProcessor():
         plt.plot(self.x_data, self.average_y_data, label = "Individual average of tweets per 10")
         plt.draw()
 
-        # Perform averaging and plot
-        self.normalize(response, average_step)
-
-        # Perhaps putting into a method changes something about calculations? (It shouldn't?)
-        # if self.average_count != average_step:
-        #     self.current_average += response
-        #     self.average_count += 1
-        # else:
-        #     self.hashtag[self.name] += self.current_average / self.average_count
-        #
-        #     # Append data
-        #     self.total_step_count += 1
-        #     self.x_data.append(self.total_step_count * 10)
-        #     self.average_y_data.append(self.current_average / self.average_count)
-        #     self.trend_y_data.append(self.hashtag[self.name])
-        #
-        #     # Reset
-        #     self.current_average = 0
-        #     self.average_count = 0
-        #     self.plot()
-
-
-
+        # Perform averaging and plot and store data into csv file
+        self.normalize(text, response, average_step, database)
 
     def get_text(self, raw_data):
         try: return json.loads(raw_data)['text']
@@ -79,7 +61,7 @@ class TextProcessor():
         plt.pause(0.1)
 
 
-    def normalize(self, response, average_step):
+    def normalize(self, text, response, average_step, database):
         if self.average_count != average_step:
             self.current_average += response
             self.average_count += 1
@@ -91,11 +73,13 @@ class TextProcessor():
             self.x_data.append(self.total_step_count * 10)
             self.average_y_data.append(self.current_average / self.average_count)
             self.trend_y_data.append(self.hashtag[self.name])
+            csv.write("{} ..., {}, {} \n".format(text[0:20], response, str(dt.datetime.now())))
 
             # Reset
             self.current_average = 0
             self.average_count = 0
             self.plot()
+
 
     def clean(self, text):
         ## TODO: may be useful to ignore certain tweets and use others, i.e rt
